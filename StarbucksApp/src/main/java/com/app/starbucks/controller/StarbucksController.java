6b587916908497e80358edcd9e2cdaf239c271e7
Harini Balakrishnan <hariniemmawatson@gmail.com>
2018-07-24 10:52:18 +0530

    Add JAR library file and implement MyCards GET APIs

--- StarbucksApp/src/main/java/com/app/starbucks/controller/StarbucksController.java
+++ StarbucksApp/src/main/java/com/app/starbucks/controller/StarbucksController.java
@@ -3,7 +3,11 @@ package com.app.starbucks.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import starbucks.*;
@@ -13,12 +17,19 @@ import java.sql.Connection;
 @RestController
 public class StarbucksController {
 	starbucks.ManagePayments mp = new starbucks.ManagePayments();
-	com.starbucks.library.MySqlConnection mysql = new  com.starbucks.library.MySqlConnection();
-	Connection connection=  mysql.getConnection();
-	com.starbucks.library.MyCards  mycards;
-	com.starbucks.library.AddCard addcard;
-	com.starbucks.library.Card card;
 	List<starbucks.Payment> paymentsList = new ArrayList<>();
+	
+//	  Author - Harini Balakrishnan 
+// 	  Database credentials and initialize classes from AddCard JAR file	
+	com.starbucks.library.MySqlConnection mysql = new  com.starbucks.library.MySqlConnection();
+	String url = "";    
+    String username = ""; 
+    String password = "";
+    Connection connection = mysql.getConnection(url, username, password);
+	com.starbucks.library.MyCards  mycards = new com.starbucks.library.MyCards();
+	com.starbucks.library.AddCard addcard = new com.starbucks.library.AddCard();
+	com.starbucks.library.Card card = new com.starbucks.library.Card();
+	
 	
 	@RequestMapping("/")
     public String home(){
@@ -35,24 +46,18 @@ public class StarbucksController {
 	}
 	
 	
+//	  AddCard API - Harini Balakrishnan 
+//	  GET - current active card
 	@RequestMapping("/mycards/active")
-	public String getActiveCard() {
-		mycards = new MyCards();
-		return mycards.getActiveCard().getCardID();
+	public  com.starbucks.library.Card getActiveCard() {
+		return mycards.getActiveCard();
 	}
 	
-	
+//	  AddCard API - Harini Balakrishnan 
+//	  GET - all cards in the table
 	@RequestMapping("/mycards/all")
 	public ArrayList<com.starbucks.library.Card> getAllCards() {
-		mycards = new MyCards();
 		return mycards.getAllCards();
 	}
-	
-	@PostMapping("/addcard")
-	public void insert() {
-		addcard = new AddCard();
-		addcard.insertCard("987654321", "123", 20.00, 1, true);
-    }
-	
 	
 }
